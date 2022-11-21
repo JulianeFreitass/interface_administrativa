bf26642330a4264b0945fbb038065a164502fa40
JulianeFreitass <juliane.freitas55@gmail.com>
2022-11-21 20:38:09 -0300

    Adicionando teste com jest

---
+++ test/models.test.js
@@ -0,0 +1,9 @@
+const model = require('../src/models/models');
+var Projeto = model.Projeto;
+var prj = new Projeto(1,'Teste CI','Software','JavaScript','01/01/01','02/01/01');
+
+test('Verifica o setter de uma propriedade', () => {
+  expect(typeof prj.id).toBe("number");
+  expect(prj.titulo && prj.description !== null).toBe(true);
+  expect(prj.inicio < prj.fim).toBe(true);
+});
